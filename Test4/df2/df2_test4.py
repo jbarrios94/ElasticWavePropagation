d80a3d806f14e18b6e3d82e5cb1ddbdf336a14f9
Juan Camilo Barrios Camargo <59795706+jbarrios94@users.noreply.github.com>
2024-04-30 21:05:41 -0300

    Add files via upload

---
+++ Test4/df2/df2_test4.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Juan Barrios
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import Plot as gf
+
+ref_factor = 0
+c1 = np.load("gdm_rf" + str(ref_factor) + ".npy")
+so = 2
+so2 = so//2
+nx  = c1.shape[1]
+nz  = c1.shape[0]  
+xmin = 20000.
+xmax  = 50000.
+zmin = 0
+zmax  = 8000.
+tmax  = 5000.
+dx = (xmax - xmin) / (nx - 1)
+dz = (zmax - zmin) / (nz - 1)
+x  = (np.arange(nx)*dx)
+z  = (np.arange(nz)*dz)
+isx = round(nx + 2*so)//2 
+isz = np.where(np.linspace(zmin, zmax, nz) == 32)[0][0] + so
+
+c = np.zeros((nx + 2*so, nz + 2*so))
+c[so:-so, so:-so] = c1[:, :].T
+
+for i in range(so):
+    c[i, :] = c[so, :]
+    c[:, i] = c[:, so]
+    
+for i in range(nx + so, nx + 2*so):
+    c[i, :] = c[nx + so - 1, :]
+
+for i in range(nz + so, nz + 2*so):
+    c[:, i] = c[:, nz + so - 1]
+
+
+b = 1 / (0.31 * (1e3*c)**0.25)
+b[c < 1.51] = 1.0
+rho = 1/b
+k = c**2 * rho    
+
+cfl = 0.5
+dt = np.minimum(dx, dz) * cfl / np.max(c)
+nt = round(tmax/dt)                   
+
+f0  = 0.005 
+t0 = 1 / f0
+time_values = np.arange(nt)*dt
+a = 1
+r = (np.pi * f0 * (time_values - t0))
+src = a * (1-2.*r**2)*np.exp(-r**2)
+
+v_x = np.zeros((nx + 2*so, nz + 2*so))                                     
+v_y = np.zeros((nx + 2*so, nz + 2*so))                                   
+p = np.zeros((nx + 2*so, nz + 2*so)) 
+receiver = np.zeros((nt, nx))    
+                                
+start = time.time()
+
+r4 = 1/dx
+r5 = 1/dz
+for it in range(nt):
+    #----------------------------------Df2-----------------------------------
+    p[2:-2, 2:-2] = 0.5*dt*(-r4*(b[1:-3, 2:-2] + b[2:-2, 2:-2])*v_x[1:-3, 2:-2] 
+                            + r4*(b[2:-2, 2:-2] + b[3:-1, 2:-2])*v_x[2:-2, 2:-2] 
+                            - r5*(b[2:-2, 1:-3] + b[2:-2, 2:-2])*v_y[2:-2, 1:-3] 
+                            + r5*(b[2:-2, 2:-2] + b[2:-2, 3:-1])*v_y[2:-2, 2:-2]) + p[2:-2, 2:-2]
+    
+    # j = so - 1
+    # for i in range(so, 2*so):
+    #     p[j, :] = p[i, :]
+    #     j = j - 1
+    
+    # j = nx + so - 1
+    # for i in range(nx + so, nx + 2*so):
+    #     p[i, :] = p[j, :]
+    #     j = j -1
+        
+    j = so - 1
+    for i in range(so, 2*so):
+        p[:, j] = p[:, i]
+        j = j - 1
+        
+    # j = nz + so - 1
+    # for i in range(nz + so, nz + 2*so):
+    #     p[:, i] = p[:, j]
+    #     j = j - 1
+        
+    r6 = c[2:-1, 2:-1]*c[2:-1, 2:-1]*p[2:-1, 2:-1]/b[2:-1, 2:-1]
+    
+    v_x[2:-2, 2:-2] = dt*(r4*(-r6[:-1, :-1]) + r4*r6[1:, :-1]) + v_x[2:-2, 2:-2]
+    v_y[2:-2, 2:-2] = dt*(r5*(-r6[:-1, :-1]) + r5*r6[:-1, 1:]) + v_y[2:-2, 2:-2]
+    
+    p[isx, isz] = p[isx, isz] + dt * src[it] / (dx * dz)
+    
+    j = so - 1
+    for i in range(so, 2*so):
+        v_x[:, j] = v_x[:, i]
+        v_y[:, j] = -v_y[:, i]
+        j = j - 1
+        
+    # j = nz + so - 1
+    # for i in range(nz + so, nz + 2*so):
+    #     v_x[:, i] = v_x[:, j]
+    #     v_y[:, i] = -v_y[:, j]
+    #     j = j - 1
+    
+    # j = so - 1
+    # for i in range(so, 2*so):
+    #     v_x[j, :] = -v_x[i, :]
+    #     v_y[j, :] = v_y[i, :]
+    #     j = j - 1
+    # j = nx + so - 1
+    # for i in range(nx + so, nx + 2*so):
+    #     v_x[i, :] = -v_x[j, :]
+    #     v_y[i, :] = v_y[j, :]
+    #     j = j -1
+        
+    receiver[it, :] = p[so:-so, isz].T * k[so:-so, isz].T
+
+end = time.time()
+comp_time = end - start
+domain = [xmin, xmax, zmin, zmax]
+gf.plot_image(p[so:-so, so:-so].T * k[so:-so, isz].T, extent = [xmin, xmax, zmax, zmin])
+is1 = np.where(np.linspace(xmin, xmax, nx) == 33350)[0][0]
+gf.plot_seismic_traces([receiver[:, is1]], 0, tmax)
+gf.plot_shotrecord(receiver[:,:], (domain[0],domain[2]), (domain[1], domain[3]), 0, tmax, factor=100)
+plt.show()
---
+++ Test4/fwpa/fwpa_teste5.py
@@ -0,0 +1,346 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Juan Barrios
+"""
+
+import time
+import numpy             as np
+import matplotlib.pyplot as plt
+import Plot as gf
+
+def Domain(LimInfx, LimSupx, LimInfy, LimSupy, NumVolx, NumVoly):
+    dx = (LimSupx - LimInfx)/(NumVolx - 1)
+    dy = (LimSupy - LimInfy)/(NumVoly - 1)
+    xn = np.linspace(LimInfx, LimSupx + dx, NumVolx + 1)
+    yn = np.linspace(LimInfy, LimSupy + dy, NumVoly + 1)
+    xc = np.zeros(NumVolx + 4)
+    yc = np.zeros(NumVoly + 4)
+    xc[2:-2] = xn[:-1]
+    yc[2:-2] = yn[:-1]
+    Xc, Yc = np.meshgrid(xc,yc)
+    Xn, Yn = np.meshgrid(xn, yn)
+    return Xc, Yc, Xn, Yn, dx, dy
+
+def Riemann_Elasticity_2Dx(q, rho, k, c):
+    r11 = 1/np.sqrt(k*rho)[:,:-1]
+    r13 = -1/np.sqrt(k*rho)[:,1:]
+    dfq1 = -((q[1]/rho)[:,1:] - (q[1]/rho)[:,:-1])
+    dfq2 = -((q[0]*k)[:,1:] - (q[0]*k)[:,:-1])
+    betha1 = (dfq1 - r13*dfq2)/(r11 - r13)
+    betha3 = (-dfq1 + r11*dfq2)/(r11 - r13)
+    W1 = np.zeros(dimx)
+    W3 = np.zeros(dimx)
+    W1[0] = betha1*r11
+    W1[1] = betha1
+    W3[0] = betha3*r13
+    W3[1] = betha3
+    return W1, W3
+
+
+def Riemann_Elasticity_2Dy(q, rho, k, c):
+    r11 = 1/np.sqrt(k*rho)[:-1,:]
+    r13 = -1/np.sqrt(k*rho)[1:,:]
+    dgq1 = -((q[2]/rho)[1:,:] - (q[2]/rho)[:-1,:])
+    dgq3= -((q[0]*k)[1:,:] - (q[0]*k)[:-1,:])
+    betha1 = (dgq1 - r13*dgq3)/(r11 - r13)
+    betha3 = (-dgq1 + r11*dgq3)/(r11 - r13)
+    W1 = np.zeros(dimy)
+    W3 = np.zeros(dimy)
+    W1[0] = betha1*r11
+    W1[2] = betha1
+    W3[0] = betha3*r13
+    W3[2] = betha3
+    return W1, W3
+
+def Transvese_Riemann_Elasticity_2Dx(q, rho, k, Ap, Am, c):
+    r11 = (1/np.sqrt(k*rho))[:-2,1:]
+    r13 = (-1/np.sqrt(k*rho))[1:-1:,1:]
+    gamma1 = (Ap[0,1:-1,:] - r13*Ap[2,1:-1,:])/(r11 - r13)
+    dim = np.shape(r11)
+    BmAp = np.zeros((3,dim[0],dim[1]))
+    BmAp[0] = -c[:-2,1:]*gamma1*r11
+    BmAp[2] = -c[:-2,1:]*gamma1
+
+    r11 = (1/np.sqrt(k*rho))[1:-1,1:]
+    r13 = (-1/np.sqrt(k*rho))[2:,1:]
+    gamma3 = (-Ap[0,1:-1,:] + r11*Ap[2,1:-1,:])/(r11 - r13)
+    dim = np.shape(r11)
+    BpAp = np.zeros((3,dim[0],dim[1]))
+    BpAp[0] = c[2:,1:]*gamma3*r13
+    BpAp[2] = c[2:,1:]*gamma3
+    
+    r11 = (1/np.sqrt(k*rho))[:-2,:-1]
+    r13 = (-1/np.sqrt(k*rho))[1:-1,:-1]
+    gamma1 = (Am[0,1:-1,:] - r13*Am[2,1:-1,:])/(r11 - r13)
+    dim = np.shape(r11)
+    BmAm = np.zeros((3,dim[0],dim[1]))
+    BmAm[0] = -c[:-2,:-1]*gamma1*r11
+    BmAm[2] = -c[:-2,:-1]*gamma1
+    
+    r11 = (1/np.sqrt(k*rho))[1:-1,:-1]
+    r13 = (-1/np.sqrt(k*rho))[2:,:-1]
+    gamma3 = (-Am[0,1:-1,:] + r11*Am[2,1:-1,:])/(r11 - r13)
+    dim = np.shape(r11)
+    BpAm = np.zeros((3,dim[0],dim[1]))
+    BpAm[0] = c[2:,:-1]*gamma3*r13
+    BpAm[2] = c[2:,:-1]*gamma3
+    return BpAp, BmAp, BpAm, BmAm
+
+
+def Transvese_Riemann_Elasticity_2Dy(q, rho, k, Bp, Bm, c):
+    r11 = (1/np.sqrt(k*rho))[1:,:-2]
+    r13 = (-1/np.sqrt(k*rho))[1:,1:-1]
+    gamma1 = (Bp[0,:,1:-1] - r13*Bp[1,:,1:-1])/(r11 - r13)
+    dim = np.shape(r11)
+    AmBp = np.zeros((3,dim[0],dim[1]))
+    AmBp[0] = -c[1:,:-2]*gamma1*r11
+    AmBp[1] = -c[1:,:-2]*gamma1
+    
+    r11 = (1/np.sqrt(k*rho))[1:,1:-1]
+    r13 = (-1/np.sqrt(k*rho))[1:,2:]
+    gamma3 = (-Bp[0,:,1:-1] + r11*Bp[1,:,1:-1])/(r11 - r13)
+    dim = np.shape(r11)
+    ApBp = np.zeros((3,dim[0],dim[1]))
+    ApBp[0] = c[1:,2:]*gamma3*r13
+    ApBp[1] = c[1:,2:]*gamma3
+    
+    r11 = (1/np.sqrt(k*rho))[:-1,:-2]
+    r13 = (-1/np.sqrt(k*rho))[:-1,1:-1]
+    gamma1 = (Bm[0,:,1:-1] - r13*Bm[1,:,1:-1])/(r11 - r13)
+    dim = np.shape(r11)
+    AmBm = np.zeros((3,dim[0],dim[1]))
+    AmBm[0] = -c[:-1,:-2]*gamma1*r11
+    AmBm[1] = -c[:-1,:-2]*gamma1
+    
+    r11 = (1/np.sqrt(k*rho))[:-1,1:-1]
+    r13 = (-1/np.sqrt(k*rho))[:-1,2:]
+    gamma3 = (-Bm[0,:,1:-1] + r11*Bm[1,:,1:-1])/(r11 - r13)
+    dim = np.shape(r11)
+    ApBm = np.zeros((3,dim[0],dim[1]))
+    ApBm[0] = c[:-1,2:]*gamma3*r13
+    ApBm[1] = c[:-1,2:]*gamma3
+    return ApBp, AmBp, ApBm, AmBm
+
+def Fimm2Dx(s, W1, W2):
+    dim = np.shape(W1)
+    F = np.zeros((dim[0], dim[1],dim[2] - 2))
+    norm = np.sum(W1[:,:,1:-1]**2,0)
+    norm += (norm == 0. ) * 1.
+    theta = np.sum(W1[:,:,2:]*W1[:,:,1:-1],0)/norm
+    W1L = SuperBee(theta, CFL)*W1[:,:,1:-1]
+    norm = np.sum(W2[:,:,1:-1]**2,0)
+    norm += (norm == 0 ) * 1.
+    theta = np.sum(W2[:,:,0:-2]*W2[:,:,1:-1],0)/norm
+    W2L = SuperBee(theta, CFL)*W2[:,:,1:-1]
+    F[:,...] = ((-(1.-dt/dx*s[:,1:-2])*W1L) +
+        ((1.-dt/dx*s[:,2:-1])*W2L))
+    F = F * 0.5
+    return F
+
+def Fimm2Dy(s, W1, W2):
+    dim = np.shape(W1)
+    F = np.zeros((dim[0], dim[1] - 2,dim[2]))
+    norm = np.sum(W1[:,1:-1,:]**2,0)
+    norm += (norm == 0. ) * 1.
+    theta = np.sum(W1[:,2:,:]*W1[:,1:-1,:],0)/norm
+    W1L = SuperBee(theta, CFL)*W1[:,1:-1,:]
+    norm = np.sum(W2[:,1:-1,:]**2,0)
+    norm += (norm == 0 ) * 1.
+    theta = np.sum(W2[:,0:-2,:]*W2[:,1:-1,:],0)/norm
+    W2L = SuperBee(theta, CFL)*W2[:,1:-1,:]
+    F[:,...] = ((-(1.-dt/dx*s[1:-2,:])*W1L) +
+        ((1.-dt/dx*s[2:-1,:])*W2L))
+    F = F * 0.5
+    return F
+
+def SuperBee(theta, cfl):
+    shape = np.shape(theta)
+    a = np.zeros(shape)
+    b = np.ones(shape)
+    c = np.min([b , 2.*theta],0)
+    d = np.min([2.*b, theta],0)
+    phy = np.max([a,c,d],0)
+    return phy
+
+
+def Ultrabee(theta, cfl):
+    shape = np.shape(theta)
+    ccfl = np.ones(shape) * cfl
+    a = np.ones(shape) * 0.001
+    b = np.ones(shape) * 0.999
+    c = np.ones(shape)
+    cfmod1 = np.max([a, ccfl], 0)
+    cfmod2 = np.min([b, ccfl], 0)
+    a = 2 * theta/cfmod1
+    T1 = np.min([c, a], 0)
+    b = 2/(1 - cfmod2)
+    T2 = np.min([theta, b], 0)
+    return np.max([np.zeros(shape), T1, T2], 0)
+
+def MC(theta):
+    shape = np.shape(theta)
+    a = np.zeros(shape)
+    b = np.ones(shape)
+    c = np.min([(b + theta)/2. , 2.*theta, 2*b],0)
+    phy = np.max([a,c],0)
+    return phy
+
+def MaxMod(a):
+    k1 = a > 0.
+    k2 = a < 0.
+    return (k1.all(0))*np.max(a,0) + (k2.all(0))*np.min(a,0)
+
+def MinMod(a):
+    k1 = a > 0.
+    k2 = a < 0.
+    return (k1.all(0))*np.min(a,0) + (k2.all(0))*np.max(a,0)
+
+def BCx(q, op, op1):
+    if op1 == 0:
+        if op == 1:
+            q[:,:,0] = q[:,:,-4]
+            q[:,:,1] = q[:,:,-3]
+        if op == 2:
+            q[:,:,0] = q[:,:,2]
+            q[:,:,1] = q[:,:,2]
+            return q
+        if op == 3:
+            q[:,:,1] = q[:,:,2]
+            q[:,:,0] = q[:,:,3]
+            q[1,:,0] = -q[1,:,3] 
+            q[1,:,1] = -q[1,:,2]
+            return q
+    if op1 == 1:
+        if op == 1:
+            q[:,:,-1] = q[:,:,3]
+            q[:,:,-2] = q[:,:,2]
+        if op == 2:
+            q[:,:,-1] = q[:,:,-3]
+            q[:,:,-2] = q[:,:,-3]
+            return q
+        if op == 3:
+            q[:,:,-2] = q[:,:,-3]
+            q[:,:,-1] = q[:,:,-4]
+            q[1,:,-1] = -q[1,:,-4] 
+            q[1,:,-2] = -q[1,:,-3]
+            return q
+
+def BCy(q, op, op1):
+    if op1 == 0:
+        if op == 1:
+            q[:,0,:] = q[:,-4,:]
+            q[:,1,:] = q[:,-3,:]
+        if op == 2:
+            q[:,0,:] = q[:,2,:]
+            q[:,1,:] = q[:,2,:]
+            return q
+        if op == 3:
+            q[:,1,:] = q[:,2,:]
+            q[:,0,:] = q[:,3,:]
+            q[2,0,:] = -q[2,3,:] 
+            q[2,1,:] = -q[2,2,:]
+            return q
+    if op1 == 1:
+        if op == 1:
+            q[:,-1,:] = q[:,3,:]
+            q[:,-2,:] = q[:,2,:]
+        if op == 2:
+            q[:,-1,:] = q[:,-3,:]
+            q[:,-2,:] = q[:,-3,:]
+            return q
+        if op == 3:
+            q[:,-1,:] = q[:,-4,:]
+            q[:,-2,:] = q[:,-3,:]
+            q[2,-1,:] = -q[2,-4,:] 
+            q[2,-2,:] = -q[2,-3,:]
+            return q
+
+ref_factor = 0
+c1 = np.load("gdm_rf" + str(ref_factor) + ".npy")
+Lim_Infx = 20000; Lim_Supx = 50000; NumVolx = c1.shape[1]
+Lim_Infy = 0; Lim_Supy = 8000; NumVoly = c1.shape[0]
+t_inicial = 0.; t_final = 5000
+CFL = 0.5
+Xc, Yc, Xn, Yn, dx, dy = Domain(Lim_Infx, Lim_Supx, Lim_Infy, Lim_Supy, NumVolx, NumVoly)
+c = np.ones((NumVoly + 4, NumVolx + 4))
+c[2:-2, 2:-2] = c1[:, :]
+c[0,:] = c[2,:]; c[-1,:] = c[-3,:]
+c[1,:] = c[2,:]; c[-2,:] = c[-3,:]
+c[:,0] = c[:,2]; c[:,-1] = c[:,-3]
+c[:,1] = c[:,2]; c[:,-2] = c[:,-3]
+        
+b = 1 / (0.31 * (1e3*c)**0.25)
+b[c < 1.51] = 1.0
+Rho = 1/b
+K = c**2 * Rho
+
+VelMax = np.max(c)
+dt = (np.minimum(dx, dy) * CFL)/VelMax 
+
+q = np.zeros((3,NumVoly + 4,NumVolx + 4))
+q = BCx(q, 3, 0)
+q = BCx(q, 3, 1)
+q = BCy(q, 3, 1)
+q = BCy(q, 3, 0)
+qb = np.copy(q) 
+Nt = int(round(t_final/dt))
+receiver = np.zeros((Nt, NumVolx))
+dim = np.shape(q)
+dimx = (dim[0], dim[1], dim[2]-1)
+dimy = (dim[0], dim[1]-1, dim[2])
+isx = NumVolx//2
+isy = np.where(np.linspace(Lim_Infy, Lim_Supy, NumVoly) == 32)[0][0]
+time_values = np.arange(Nt)*dt
+f0  = 0.005
+t0 = 1./f0
+a = 1.
+r = (np.pi * f0 * (time_values - t0))
+src = a * (1-2.*r**2)*np.exp(-r**2)
+
+start = time.time()
+
+for i in range(Nt):
+    F = np.zeros((3,NumVoly+4,NumVolx+3))
+    G = np.zeros((3,NumVoly+3,NumVolx+4))
+    W1, W3 = Riemann_Elasticity_2Dx(q, Rho, K, c)
+    F[:,:,1:-1] = F[:,:,1:-1] + Fimm2Dx(c, W1, W3)
+    BpAp, BmAp, BpAm, BmAm = Transvese_Riemann_Elasticity_2Dx(q, Rho, K, W3, W1, c)
+    G[:,:-1,1:] = G[:,:-1,1:] - dt/(2.*dx)*BmAp
+    G[:,1:,1:] = G[:,1:,1:] - dt/(2.*dx)*BpAp
+    G[:,:-1,:-1] = G[:,:-1,:-1] - dt/(2.*dx)*BmAm
+    G[:,1:,:-1] = G[:,1:,:-1] - dt/(2.*dx)*BpAm
+    
+    qb[:,:,2:-2] = qb[:,:,2:-2] - dt/dx * (W3[:,:,1:-2] + W1[:,:,2:-1])
+    
+    W1, W3 = Riemann_Elasticity_2Dy(q, Rho, K, c)
+    G[:,1:-1,:] = G[:,1:-1,:] + Fimm2Dy(c, W1, W3)
+    ApBp, AmBp, ApBm, AmBm = Transvese_Riemann_Elasticity_2Dy(q, Rho, K, W3, W1, c)
+    F[:,1:,:-1] = F[:,1:,:-1] - dt/(2.*dy)*AmBp
+    F[:,1:,1:] = F[:,1:,1:] - dt/(2.*dy)*ApBp
+    F[:,:-1,:-1] = F[:,:-1,:-1] - dt/(2.*dy)*AmBm
+    F[:,:-1,1:] = F[:,:-1,1:] - dt/(2.*dy)*ApBm
+    
+    qb[:,2:-2:,:] = qb[:,2:-2:,:] - dt/dy * (W3[:,1:-2,:] + W1[:,2:-1,:])
+    
+    qb[:, 2:-2, 2:-2] = qb[:, 2:-2, 2:-2] - dt/dx * (F[:,2:-2,2:-1] - F[:,2:-2,1:-2]) - dt/dy * (G[:,2:-1,2:-2] - G[:,1:-2,2:-2])
+    
+    qb[0, isy + 2, isx + 2] = qb[0, isy + 2, isx + 2] + dt * src[i] / (dx * dy)
+    
+    receiver[i, :] = (q[0]*K)[isy + 2, 2:-2]
+    
+    qb = BCx(qb, 3, 0)
+    qb = BCx(qb, 3, 1)
+    qb = BCy(qb, 3, 1)
+    qb = BCy(qb, 3, 0)
+    q = np.copy(qb)
+
+end = time.time()
+comp_time = end - start
+Stress = (q[0]*K)[2:-2,2:-2]
+domain = [Lim_Infx, Lim_Supx, Lim_Infy, Lim_Supy]
+gf.plot_image(Stress, extent = [Lim_Infx, Lim_Supx, Lim_Supy, Lim_Infy])
+is1 = np.where(np.linspace(Lim_Infx, Lim_Supx, NumVolx) == 33350)[0][0]
+gf.plot_seismic_traces([receiver[:, is1]], 0, t_final)
+gf.plot_shotrecord(receiver[:,:], (domain[0],domain[2]), (domain[1], domain[3]), 0, t_final, factor=100)
+plt.show()
+
